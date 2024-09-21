8ee87dc0202140da558b37a268d9d48daebf35e2
shahmaran0207 <shahmaran0207@naver.com>
2024-09-21 10:12:52 +0900

    make component

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -4,11 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+    const [inputValue,setInputValue]=useState('')
+
+    const[todoList, SetTodoList]=useState([])
+
+    const addItem=()=>{
+        SetTodoList([...todoList, inputValue])
+    }
 
   return (
       <main>
-          <input type="text"/>
-          <button>추가</button>
+          <input value={inputValue} type="text" onChange=
+              {(event)=>setInputValue((event.target.value))}/>
+          <button onClick={addItem}>추가</button>
       </main>
 
   );
